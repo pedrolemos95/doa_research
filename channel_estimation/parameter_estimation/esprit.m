e9663dbc31efa85f16bb6118d7b5ce69e6983217
Pedro Lemos <plemosp@gmail.com>
2024-03-30 20:30:39 -0300

    Continuando o estimador com score

--- channel_estimation/parameter_estimation/esprit.m
+++ channel_estimation/parameter_estimation/esprit.m
@@ -1,12 +1,12 @@
 function [parameters, weight] = esprit(varargin)
-    % INPUT: channel_observation. OUTPUT: path_doa, path_weight
+    % INPUT: channel_observation, dimensions, num_of_paths. OUTPUT: parameters, path_weight
     % Reference: Sec 5.3 from Richter
     if isempty(varargin)
         run_unitary_test();
         return;
     end
     
-    [X, dim] = parse_input_parameters(varargin, ["channel_observation", "dimensions"]);
+    [X, dim, P] = parse_input_parameters(varargin, ["channel_observation", "dimensions", "num_of_paths"]);
 
     L = numel(X(1,:));
     Rxx = (X*X')/L;
@@ -14,13 +14,12 @@ function [parameters, weight] = esprit(varargin)
     [Q,D] = eig(Rxx);
     [D,I]=sort(diag(D),1,'descend');
     Q = Q(:,I); % Sort the eigenvectors to put signal eigenvector first
-    Qs = Q(:,1); % Extract the signal subspace
+    Qs = Q(:,1:P); % Extract the signal subspace
 
     M1 = dim(1);
     M2 = dim(2);
     M3 = dim(3);
     M = M1*M2*M3;
-    weight = sqrt(D(1)/M);
 
     % selection matrices...
     Js1_1 = kron(kron(eye(M3), eye(M2)), [eye(M1-1) zeros(M1-1,1)]);
@@ -32,9 +31,9 @@ function [parameters, weight] = esprit(varargin)
     Js2_3 = kron(kron([zeros(M3-1,1) eye(M3-1)], eye(M2)), eye(M1));
 
     % Estimate mu_1, mu_2 and mu_3
-    if (M1 > 1); Psi_1 = inv((Js1_1*Qs)'*Js1_1*Qs)*(Js1_1*Qs)'*Js2_1*Qs; else; Psi_1 = 0; end
-    if (M2 > 1); Psi_2 = inv((Js1_2*Qs)'*Js1_2*Qs)*(Js1_2*Qs)'*Js2_2*Qs; else; Psi_2 = 0; end
-    if (M3 > 1); Psi_3 = inv((Js1_3*Qs)'*Js1_3*Qs)*(Js1_3*Qs)'*Js2_3*Qs; else; Psi_3 = 0; end
+    if (M1 > 1); Psi_1 = inv((Js1_1*Qs)'*Js1_1*Qs)*(Js1_1*Qs)'*Js2_1*Qs; else; Psi_1 = zeros(P); end
+    if (M2 > 1); Psi_2 = inv((Js1_2*Qs)'*Js1_2*Qs)*(Js1_2*Qs)'*Js2_2*Qs; else; Psi_2 = zeros(P); end
+    if (M3 > 1); Psi_3 = inv((Js1_3*Qs)'*Js1_3*Qs)*(Js1_3*Qs)'*Js2_3*Qs; else; Psi_3 = zeros(P); end
 
     [~,mu_1] = eig(Psi_1);
     mu_1 = diag(-angle(mu_1));
@@ -44,21 +43,26 @@ function [parameters, weight] = esprit(varargin)
     mu_3 = diag(-angle(mu_3));
 
     parameters = [mu_1;mu_2;mu_3];
+    % Best Linear Unbiased Estimator with white noise assumption. Richter Sec. 5.1
+    B = specular_model(parameters, dim);
+    weight = mean(inv(B'*B)*B'*X,2); % TODO: Check if this mean is ok
 end
 
 function run_unitary_test()
     delay = 1e-9;
     doa = [45; 30]*(pi/180);
     mu = parameter_mapping([delay;doa], "physical");
-    path_weight = 1;
+    path_weight = 1+0.5*1i;
+
+    P = 1; % Paths to be estimated
 
     % aperture dimensions
-    M1 = 1; % number of frequencies
-    M2 = 2; % number of rows in antenna array
+    M1 = 20; % number of frequencies
+    M2 = 4; % number of rows in antenna array
     M3 = 4; % number of cols in antenna array
     dim = [M1;M2;M3];
 
-    X = specular_model(mu, dim)*path_weight + wgn(M1*M2*M3,1,-10,'dBW');
+    X = specular_model(mu, dim)*path_weight + wgn(M1*M2*M3,1,-20,'dBW');
 
-    [est_mu, est_weight] = esprit(X, dim);
+    [est_mu, est_weight] = esprit(X, dim, P);
 end
--- channel_estimation/parameter_estimation/scored_estimator.m
+++ channel_estimation/parameter_estimation/scored_estimator.m
@@ -9,21 +9,34 @@ function [parameters, weight, score] = scored_estimator(varargin)
     [X, dim] = parse_input_parameters(varargin, ["channel_observation", "dimensions"]);
 
     % first, apply subarray smoothing to decorrelate the signals. It should
-    % be done only in the frequency domain. 
+    % be done only in the frequency domain.
     % TODO: Should the smoothing be applied to stay with 2M1/3 + 1 size or size 1?
-
+    % TODO: Use forward-backward averaging?
     % All we have to do is to rearrange the observation.
     M1 = dim(1);
     M2 = dim(2);
     M3 = dim(3);
-    M = M1*M2*M3;
 
-    % TODO: Test if its not great than M
-    M1_ss = 1;
-    X_ss = reshape(X, [M1 M2*M3]).';
+    if (M1 >= 3)
+        M1_ss = ceil(2*M1/3)+1;
+    else
+        M1_ss = 1;
+    end
+
+    X_rs = reshape(X, [M1 M2*M3]);
+
+    Md = M1 - M1_ss + 1;
+
+    X_ss = zeros(M1_ss*M2*M3, Md);
+    for n=1:Md
+        tmp = X_rs(n:M1_ss+n-1,:);
+        X_ss(:,n) = tmp(:);
+    end
 
     dim_ss = [M1_ss;M2;M3];
-    [parameters, weight] = esprit(X_ss, dim_ss);
+    P = Md;
+
+    [parameters, weight] = esprit(X_ss, dim_ss, P);
 
     % Remove the path estimate from the observation, in order to get an
     % residue
@@ -45,9 +58,9 @@ end
 
 function run_unitary_test()
     delays = [1e-9;30e-9];
-    doas = [53; 15;20;80]*(pi/180);
+    doas = [53; 15;20;70]*(pi/180);
     mu = parameter_mapping([delays;doas], "physical");
-    path_weights = [1;0.1];
+    path_weights = [1.8 - 1i*0.75;0.5*1i];
 
     % aperture dimensions
     M1 = 20; % number of frequencies
@@ -55,7 +68,7 @@ function run_unitary_test()
     M3 = 4; % number of cols in antenna array
     dim = [M1;M2;M3];
 
-    X = specular_model(mu, dim)*path_weights + wgn(M1*M2*M3,1,-20,'dBW');
+    X = specular_model(mu, dim)*path_weights + wgn(M1*M2*M3,1,-50,'dBW');
 
     [est_mu, est_weight, score] = scored_estimator(X, dim);
 end
